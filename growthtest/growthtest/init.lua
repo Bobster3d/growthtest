77ea3d6f2d29bfdff55470bf0891f38f3293c30e
Bobster3d <Bobster3d@users.noreply.github.com>
2014-09-14 07:26:21 +0100

    gtvines
    gtvines (vine "system")
    growthtest.wallmounted_to_dir(wallmounteddir) - added so it's possible
    to get a wallmounted nodes dir vector from its dir index

--- growthtest/growthtest/init.lua
+++ growthtest/growthtest/init.lua
@@ -5,3 +5,18 @@ growthtest.modsupport = {
 	pipeworks = minetest.get_modpath("pipeworks"),
 	mesecons = minetest.get_modpath("mesecons"),
 }
+
+function growthtest.wallmounted_to_dir(wallmounteddir)
+	--a table of possible dirs
+	return ({{x=0, y=1, z=0},
+			{x=0, y=-1, z=0},
+			{x=1, y=0, z=0},
+			{x=-1, y=0, z=0},
+			{x=0, y=0, z=1},
+			{x=0, y=0, z=-1}})
+			--indexed into by a table of correlating wallmounteddir
+			[({[0]=1, 2, 3, 4, 
+				5, 6})
+			--indexed into by the wallmounteddir in question
+			[wallmounteddir]]
+end
---
+++ growthtest/gtvines/init.lua
@@ -0,0 +1,71 @@
+gtvines = { }
+gtvines.drops = { items = { } }
+function gtvines.register_vine_drop(drop)
+	for i, items in ipairs(gtvines.drops.items) do
+		for key, value in ipairs(items.items) do
+			if value == drop.item then 
+				print("vine drop exists use overwrite_vine_drop") 
+				return 
+			end
+		end
+	end
+	table.insert(gtvines.drops.items, {items = {drop.item}, rarity = drop.rarity})
+end
+
+function gtvines.overwrite_vine_drop(drop)
+	local overwrite_drop = drop
+	for i, items in ipairs(gtvines.drops.items) do
+		for key, value in ipairs(items.items) do
+			if value == overwrite_drop.item then 	
+				value = overwrite_drop.item
+				if items.rarity ~= nil then
+					items.rarity = overwrite_drop.rarity
+				end
+				print("overwrote vine drop "..items.rarity ) 
+			end
+		end
+	end
+end
+
+minetest.register_node("gtvines:vine", {
+	description = "Vine",
+	drawtype = "signlike",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	sunlight_propagates = true,
+	walkable = false,
+	climbable = true,
+	legacy_wallmounted = true,
+	selection_box = {
+		type = "wallmounted",
+	},
+	tile_images = { "gtvines_vine.png" },
+	inventory_image = "gtvines_vine.png",
+	wield_image = "gtvines_vine.png",
+	drop = gtvines.drops,
+	groups = {snappy=3,flammable=2,vines=1,attached_node=1,hanging_node=1},
+	sounds = default.node_sound_leaves_defaults(),
+	after_place_node = function(pos, placer)
+		local n = minetest.get_node_or_nil(pos)
+		local dir = n.param2
+		if dir == 0 or dir == 1 then
+			minetest.remove_node(pos)
+			return true
+		end
+		return false
+    end,
+})
+
+minetest.register_abm({
+	nodenames = {"gtvines:vine"},
+	interval = 5,
+	chance = 1,
+	action = function(pos, node, active_object_count, active_object_count_wider)
+		local dir = growthtest.wallmounted_to_dir(node.param2)
+		local posbelow = {x=pos.x, y=pos.y-1, z=pos.z}
+		local nodebelow = minetest.get_node(posbelow)
+		if nodebelow.name == "air" then
+			minetest.add_node(posbelow, {name=node.name, param2 = node.param2})
+		end
+	end
+})
